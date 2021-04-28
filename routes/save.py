21349299929395ab58e4079a2c198c823e7ad87d
ghebyonnainggolan <82504538+ghebyonnainggolan@users.noreply.github.com>
2021-04-28 11:07:49 +0700

    Update save.py

--- routes/save.py
+++ routes/save.py
@@ -1,12 +1,12 @@
 import os
 from modules.save_mod import FindFolder, MakeFolder, RemakeFile
-from modules.store import _data
+from modules.store import TakeData
 
 def Save():
     save_folder = input("Masukkan nama folder penyimpanan : ")
     Path = os.getcwd()
     print("\nSaving...")
-    if (FindFolder(save_folder, Path) == False):
+    if not FindFolder(save_folder, Path):
         MakeFolder(save_folder, Path)
 
-    RemakeFile(_data, save_folder, Path)
+    RemakeFile(TakeData(), save_folder, Path)
