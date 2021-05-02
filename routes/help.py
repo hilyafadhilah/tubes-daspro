04ed7ce32cff8934ad992b61e277f3fb9d29c294
ghebyonnainggolan <82504538+ghebyonnainggolan@users.noreply.github.com>
2021-05-02 20:14:11 +0700

    Update help.py

--- routes/help.py
+++ routes/help.py
@@ -1,31 +1,30 @@
+from modules.store import GetCurrentUser
+
 def DisplayHelp():
-    access = input("Admin/User : ") #masih belum ada fungsi buat mencari role nya
-    print("============= HELP ============")
-    if access == "Admin" :
+    user = GetCurrentUser()
+    if user['role'] == "Admin" :
         print("""
-        registrasi - untuk melakukan registrasi baru
-        login - untuk melakukan login ke dalam sistem
-        cariraritiy - untuk menampilkan gadget berdasarkan rarity
-        caritahun - untuk menampilkan gadget berdasarkan kategori tahun
-        tambahitem - untuk melakukan penambahan item
-        hapusitem - untuk menghapus item dalam database
-        ubahjumlah - untuk menambahkan atau membuang sejumlah gadget
-        riwayatpinjam - untuk menampilkan sejumlah riwayat peminjaman terbaru
-        riwayatkembali - untuk menampilkan sejumlah riwayat pengembalian gadget terbaru
-        riwayatambil - untuk menampilkan sejumlah riwajat pengambilan consumable terbaru
-        save - menyimpan data ke dalam file setelah dilakukan perubahan
-        help - menampilkan panduan penggunaan sistem
-        exit - keluar dari aplikasi
-        """)
-    elif access == "User":
+registrasi - untuk melakukan registrasi baru
+login - untuk melakukan login ke dalam sistem
+cariraritiy - untuk menampilkan gadget berdasarkan rarity
+caritahun - untuk menampilkan gadget berdasarkan kategori tahun
+tambahitem - untuk melakukan penambahan item
+hapusitem - untuk menghapus item dalam database
+ubahjumlah - untuk menambahkan atau membuang sejumlah gadget
+riwayatpinjam - untuk menampilkan sejumlah riwayat peminjaman terbaru
+riwayatkembali - untuk menampilkan sejumlah riwayat pengembalian gadget terbaru
+riwayatambil - untuk menampilkan sejumlah riwajat pengambilan consumable terbaru
+save - menyimpan data ke dalam file setelah dilakukan perubahan
+help - menampilkan panduan penggunaan sistem
+exit - keluar dari aplikasi""")
+    elif user['role'] == "User":
         print("""
-        login - untuk melakukan login ke dalam sistem
-        cariraritiy - untuk menampilkan gadget berdasarkan rarity
-        caritahun - untuk menampilkan gadget berdasarkan kategori tahun
-        pinjam - untuk melakukan peminjaman gadget yang tersedia
-        kembalikan - untuk melakukan pengembalian gadget yang tersedia
-        minta - untuk meminta consumable yang tersedia
-        save - menyimpan data ke dalam file setelah dilakukan perubahan
-        help - menampilkan panduan penggunaan sistem
-        exit - keluar dari aplikasi
-        """)
+login - untuk melakukan login ke dalam sistem
+cariraritiy - untuk menampilkan gadget berdasarkan rarity
+caritahun - untuk menampilkan gadget berdasarkan kategori tahun
+pinjam - untuk melakukan peminjaman gadget yang tersedia
+kembalikan - untuk melakukan pengembalian gadget yang tersedia
+minta - untuk meminta consumable yang tersedia
+save - menyimpan data ke dalam file setelah dilakukan perubahan
+help - menampilkan panduan penggunaan sistem
+exit - keluar dari aplikasi""")
