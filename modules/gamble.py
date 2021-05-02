d1b74858fd36a91d47f7b70a7ad47c5ded1e2fd9
hilyafadhilah <hilyafadhilah@gmail.com>
2021-05-02 18:39:37 +0700

    feat: gacha module and route

---
+++ modules/gamble.py
@@ -0,0 +1,89 @@
+import time
+import math
+from modules.constants import baseGachaPoints, pointsBoost
+from modules.utils import ListFilter
+
+def Random():
+    m = 2 ** 32
+    a = 22695477
+    c = 1
+
+    time.sleep(0.001)
+
+    xn = time.time()
+    n = int(xn) % 16807
+
+    for i in range(n):
+        xn = (a * xn + c) % m
+    
+    return xn / m
+
+def RandomInt(min, max):
+    return min + math.floor(Random() * (max - min))
+
+def InitGachaPoints(items):
+    global baseGachaPoints
+
+    avlbl = list(set(map(lambda x: x['rarity'], items)))
+    pnts = {}
+
+    for r in baseGachaPoints:
+        if r in avlbl:
+            pnts[r] = baseGachaPoints[r]
+
+    return pnts
+
+def CalculateItemBoost(gachaPnts, item, qty):
+    global pointsBoost
+
+    boost = {}
+    ir = item['rarity']
+
+    if ir in pointsBoost:
+        for br in pointsBoost[ir]:
+            factor = RandomInt(1, qty)
+            boost[br] = pointsBoost[ir][br] * factor
+
+    return boost
+
+def AddBoost(gachaPnts, boost):
+    pnts = {}
+
+    for br in gachaPnts:
+        pnts[br] = gachaPnts[br]
+        if br in boost:
+            pnts[br] += boost[br]
+
+    return pnts
+
+def NormalizeGachaPoints(gachaPnts):
+    pnts = {}
+    low = min(gachaPnts.values())
+
+    for r in gachaPnts:
+        pnts[r] = gachaPnts[r]
+        if low < 0:
+            pnts[r] += abs(low)
+
+    return pnts
+
+def RollGacha(gachaPnts, items):
+    pnts = NormalizeGachaPoints(gachaPnts)
+    total = sum(pnts.values())
+    high = max(pnts.values())
+
+    rand = RandomInt(0, high)
+
+    for r in pnts:
+        if rand < pnts[r]:
+            rarity = r
+            break
+        rand -= pnts[r]
+
+    pool = ListFilter(items, lambda x, i: x['rarity'] == rarity)
+    idx = RandomInt(0, len(pool) - 1)
+    item = pool[idx]
+
+    qty = 1 + RandomInt(0, total / pnts[rarity])
+
+    return { 'item': pool[idx], 'qty': qty }
---
+++ routes/gacha.py
@@ -0,0 +1,117 @@
+from modules.store import GetCollection, FindOne, InsertOne, UpdateOne, GetCurrentUser
+from modules.utils import ListFilter
+from modules.gamble import Random, InitGachaPoints, CalculateItemBoost, AddBoost, RollGacha
+from modules.view import Confirm, PrintNumbered, PrintHeader
+from modules.inputs import PromptLoop, InputDate, InputInt
+import math
+
+def GachaRoute():
+    shouldAdd = True
+    consumables = GetCollection('consumable')
+    points = InitGachaPoints(consumables)
+    consumes = []
+
+    while shouldAdd:
+        PrintHeader('INVENTORY')
+        PrintNumbered(consumables, ShowEachConsumable)
+        print('')
+
+        idx = InputConsumableIndex(consumables)
+        qty = InputInt('Jumlah yang digunakan: ', min=1, max=consumables[idx]['jumlah'])
+
+        consumables[idx]['jumlah'] -= qty
+        consumes.append((idx, qty))
+
+        print(f"{consumables[idx]['nama']} (x{qty}) ditambahkan!\n")
+
+        # boost
+        boost = CalculateItemBoost(points, consumables[idx], qty)
+
+        PrintGachaPoints(points, boost)
+        points = AddBoost(points, boost)
+
+        if not Confirm('Tambahkan item lagi?'):
+            shouldAdd = False
+    
+    date = InputDate('Masukkan tanggal roll gacha: ')
+
+    PrintHeader('Rolling Gacha...')
+    result = RollGacha(points, consumables)
+
+    print(
+        f"Selamat, kamu mendapatkan {result['item']['nama']} " +
+        f"(Rarity {result['item']['rarity']}) (x{result['qty']})!"
+    )
+
+    # Masukkan ke data
+    userId = GetCurrentUser().id
+
+    for idx, qty in consumes:
+        UpdateOne('consumable', consumables[idx]['id'], {
+            'jumlah': consumables[idx]['jumlah']
+        })
+
+        InsertOne('consumable_history', autoId=True, value={
+            'id_pengambil': userId,
+            'id_consumable': consumables[idx]['id'],
+            'tanggal_pengambilan': date,
+            'jumlah': qty
+        })
+
+    UpdateOne('consumable', result['item']['id'], {
+        'jumlah': consumables[idx]['jumlah'] + qty
+    })
+
+def InputConsumableIndex(items):
+    def IsValidConsumable(inp):
+        try:
+            idx = int(inp) - 1
+            if idx < 0:
+                return False, -1
+            elif idx >= len(items):
+                return False, 1
+            elif items[idx]['jumlah'] < 1:
+                return False, items[idx]
+        except ValueError:
+            return False, None
+        return True, idx
+
+    def ErrMsg(inp, bag):
+        if bag is None:
+            print('Bukan nomor yang valid!')
+        elif bag == 1 or bag == -1:
+            print(f'Nomor harus di antara 1 - {len(items)}.')
+        else:
+            print(f"Stok item {bag['nama']} sudah habis!")
+
+    _, value = PromptLoop(
+        msg='Pilih consumable yang ingin digunakan: ',
+        until=IsValidConsumable,
+        err=ErrMsg,
+        bag=True
+    )
+
+    return value
+
+def ShowEachConsumable(consumable):
+    print(
+        consumable['nama'],
+        f"(Rarity {consumable['rarity']})",
+        f"({consumable['jumlah']})"
+    )
+
+def PrintGachaPoints(pnts, boost = None):
+    cur = AddBoost(pnts, boost)
+
+    print('Point gacha kamu:')
+    for rarity in pnts:
+        new = ''
+
+        if rarity in boost:
+            if boost[rarity] > 0:
+                new = f" (+{boost[rarity]})"
+            elif boost[rarity] < 0:
+                new = f" ({boost[rarity]})"
+
+        print(f" Rarity {rarity} : {cur[rarity]}{new}")
+    print('')
