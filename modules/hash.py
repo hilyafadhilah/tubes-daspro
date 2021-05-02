bac4c704cbfd45bc2abd11c71c249c5317186a98
ghebyonnainggolan <82504538+ghebyonnainggolan@users.noreply.github.com>
2021-05-02 20:55:44 +0700

    Update hash.py

--- modules/hash.py
+++ modules/hash.py
@@ -1,11 +1,8 @@
-def hashing(msg, what):
+def hashing(msg):
     MsgList = list(msg)
     #Spasi dimasukkan sebagai karakter
     code = "qE1~ YMUR2\"`hNIdPzi%^t@(Ao:=CQ,nx4S[7mHFye#aT6+v)DfKL$r?bkOGB>}!9_wV']jcp5JZ&Xl|\\8sg<{3.u*W-0/"
-    if what == "encrypt":
-        pswd = Encrypt(MsgList, code)
-    elif what == "decrypt":
-        pswd = Decrypt(MsgList, code)
+    pswd = Encrypt(MsgList, code)
     return pswd
 
 
@@ -30,25 +27,3 @@ def Encrypt(msg, code):
         encrypt.append(code[hasil])
     
     return "".join(encrypt)
-
-def Decrypt(msg, code):
-    decrypt = []
-    IdxAwal = 0
-    IdxAkhir = 0
-    
-    for i in range (len(msg)):
-        FindIdx = 0
-        while True:
-            if(msg[i] == code[FindIdx]):
-                NilaiAwal = FindIdx + 1
-                break
-            else:
-                FindIdx += 1
-        
-        IdxAwal -= 1
-        IdxAkhir += IdxAwal
-        NilaiAkhir = NilaiAwal + IdxAkhir
-        hasil = (94*1000000+(NilaiAkhir-1))%94
-        decrypt.append(code[hasil])
-    
-    return "".join(decrypt)
